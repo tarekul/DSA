53a8bf0245a163215654777b9d703ad8d83baf8d
Pursuit <pursuit@Pursuits-Air.lan>
2020-08-06 02:11:11 -0400

    queue using linked list

---
+++ DataStructures/queue.js
@@ -0,0 +1,43 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(val) {
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.size++;
+  }
+
+  dequeue() {
+    if (!this.first) return null;
+
+    const temp = this.first;
+    if (this.first === this.last) this.last = null;
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
+
+const q = new Queue();
+q.enqueue("hello");
+q.enqueue("sir");
+q.enqueue("good evening");
+q.dequeue();
+console.log(q.first);
