82a45fc4c3c737ea25c48a60e893bc43c6fb59f3
Pursuit <pursuit@Pursuits-Air.lan>
2020-08-06 23:26:15 -0400

    added notes on time complexity for insert and find

--- DataStructures/binarySearchTree.js
+++ DataStructures/binarySearchTree.js
@@ -10,6 +10,24 @@ class BST {
   constructor() {
     this.root = null;
   }
+
+  /*
+  Time complexity of insert, and find
+  Worse Case  Avg Case  Best Case
+    O(n)      O(log n)    O(1)  
+
+  The worse case is O(n) is because if tree has nodes ONLY on one side from the root, such as
+    1 
+      2
+        3
+          4
+            5
+              6
+                7
+  than you have to check at worse case all the nodes to find 7.
+  Avg case is O(log n) because if you have balanced tree than you are checking from root node to the leaf node
+  and that at most will be log (n) checks. If you have 32 nodes than you will have to check at most 5 times to find node.
+  */
   insert(val) {
     const newNode = new Node(val);
     if (!this.root) this.root = newNode;
